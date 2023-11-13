2585e7e113582c3be903b7c4c44a75170d454fbc
Serg Klopenko <sergey.klopenko@gmail.com>
2023-11-13 10:29:51 +0200

    reset

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("develop-01");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Develop");
-        System.out.println("master develop-01");
-        System.out.println("master new commit");
-        System.out.println("master changed");
-        System.out.println("develop-02");
-        System.out.println("develop");
 
-        System.out.println("develop-01");
-        System.out.println("develop-02");
-        System.out.println("develop");
-
-        System.out.println("develop-01");
-        System.out.println("develop-02");
     }
 }
